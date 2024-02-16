e31673d6c00df49631401c9bceed639128d56890
VoloshynaK <ko.voloshina@gmail.com>
2024-02-16 16:06:35 -0500

    updated

---
+++ src/js/1-gallery.js
@@ -0,0 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+
+const galleryEl = document.querySelector(".gallery");
+
+const imgElMarkup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}"/>
+            
+        </a>
+    </li>`).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", imgElMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+import '../css/2-form.css';
+import '../css/common.css';
+import throttle from 'lodash.throttle';
+
+
+const form = document.querySelector("form");
+const emailInput = document.querySelector("input");
+const msgInput = document.querySelector("textarea");
+
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {
+    email: '',
+    message: '',
+}
+
+form.addEventListener("input", throttle(onInput, 500));
+form.addEventListener("submit", onFormSubmit)
+
+populateTextAreas();
+
+function onInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.clear();
+}
+
+
+
+function populateTextAreas() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedData) {
+        emailInput.value = savedData.email;
+        msgInput.value = savedData.message;
+    }
+}
